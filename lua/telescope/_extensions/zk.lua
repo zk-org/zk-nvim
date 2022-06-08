1e942d3f74aa8877dd7ed9fcc5ad8f1a3950c921
deforde <7503504+deforde@users.noreply.github.com>
2022-06-08 11:44:48 +0200

    Add tags picker to Telescope extension. (#54)

--- lua/telescope/_extensions/zk.lua
+++ lua/telescope/_extensions/zk.lua
@@ -6,8 +6,21 @@ local function show_notes(opts)
   zk.edit(opts, { picker = "telescope", telescope = opts })
 end
 
+---@param opts? table additional options for zk, telescope options, all optional and in one table
+---@see https://github.com/mickael-menu/zk/blob/main/docs/editors-integration.md#zktaglist
+local function show_tags(opts)
+  zk.pick_tags(opts, { picker = "telescope", telescope = opts }, function(tags)
+    tags = vim.tbl_map(function(v)
+      return v.name
+    end, tags)
+    opts = vim.tbl_extend("force", { tags = tags }, opts or {})
+    zk.edit(opts, { picker = "telescope", telescope = opts })
+  end)
+end
+
 return require("telescope").register_extension({
   exports = {
     notes = show_notes,
+    tags = show_tags,
   },
 })
