54eab06a23f2b6d367252caa01b96df475dc70a5
Pete Kazmier <pete@kazmier.com>
2024-04-11 18:01:33 -0400

    Refactor and add support for multi-select

--- lua/zk/pickers/minipick.lua
+++ lua/zk/pickers/minipick.lua
@@ -7,65 +7,69 @@ M.note_picker_list_api_selection = { "title", "path", "absPath" }
 M.show_note_picker = function(notes, opts, cb)
   notes = vim.tbl_map(function(note)
     local title = note.title or note.path
-    return { text = title, path = note.absPath, note = note }
+    return { text = title, path = note.absPath, value = note }
   end, notes)
-
-  opts = opts or {}
-  local minipick_opts = vim.tbl_deep_extend("force", {
-    source = {
-      items = notes,
-      name = opts.title,
-      choose = function(_)
-        return nil
-      end,
-    },
-    window = {
-      prompt_prefix = opts.title .. "> ",
-    },
-  }, opts.minipick or {})
-
-  local item = minipick.start(minipick_opts)
-  if item ~= nil then
-    cb(opts.multi_select and { item.note } or item.note)
-  end
+  H.item_picker(notes, opts, cb)
 end
 
 M.show_tag_picker = function(tags, opts, cb)
-  tags = vim.tbl_map(function(t)
-    local padded_cnt = H.ensure_text_width(t.note_count, opts.note_count_width or 4)
+  local width = H.max_note_count(tags)
+  tags = vim.tbl_map(function(tag)
+    local padded_count = H.pad(tag.note_count, width)
     return {
-      text = string.format("%s │ %s", padded_cnt, t.name),
-      tag = t,
+      text = string.format(" %s │ %s", padded_count, tag.name),
+      value = tag,
     }
   end, tags)
+  H.item_picker(tags, opts, cb)
+end
 
+H.item_picker = function(items, opts, cb)
   opts = opts or {}
   local minipick_opts = vim.tbl_deep_extend("force", {
-    source = {
-      items = tags,
-      name = opts.title,
-      choose = function(_)
-        return nil
-      end,
-    },
     window = {
       prompt_prefix = opts.title .. "> ",
     },
+    source = {
+      items = items,
+      name = opts.title,
+
+      choose = function(selected_item) -- item guaranteed to be non-nil
+        local target_window = minipick.get_picker_state().windows.target
+        vim.api.nvim_win_call(target_window, function()
+          cb(opts.multi_select and { selected_item.value } or selected_item.value)
+        end)
+        return nil
+      end,
+
+      choose_marked = function(selected_items) -- could be empty table
+        if opts.multi_select and not vim.tbl_isempty(selected_items) then
+          local target_window = minipick.get_picker_state().windows.target
+          vim.api.nvim_win_call(target_window, function()
+            cb(vim.tbl_map(function(item)
+              return item.value
+            end, selected_items))
+          end)
+        end
+        return nil
+      end,
+    },
   }, opts.minipick or {})
 
-  local item = minipick.start(minipick_opts)
-  if item ~= nil then
-    cb(opts.multi_select and { item.tag } or item.tag)
-  end
+  minipick.start(minipick_opts)
 end
 
--- From mini.extras
-H.ensure_text_width = function(text, width)
-  local text_width = vim.fn.strchars(text)
-  if text_width <= width then
-    return string.rep(" ", width - text_width) .. text
+H.max_note_count = function(tags)
+  local max_count = 0
+  for _, t in ipairs(tags) do
+    max_count = math.max(max_count, t.note_count)
   end
-  return "…" .. vim.fn.strcharpart(text, text_width - width + 1, width - 1)
+  return vim.fn.strchars(tostring(max_count))
+end
+
+H.pad = function(text, width)
+  local text_width = vim.fn.strchars(text)
+  return string.rep(" ", width - text_width) .. text
 end
 
 return M
