4c66abd610d3a6bfec6fad5066ae9cb6750face7
Shaun Fattig <shfattig@gmail.com>
2024-06-21 22:41:02 -0500

    add new grep picker
    picker uses asynchronous calls to "zk list -m <search_term>" to live filter notes based on a search term
    implemented based on Telescope's live_grep
    does not include ability to jump to line since "zk list" does not give
    this info

--- lua/zk/pickers/telescope.lua
+++ lua/zk/pickers/telescope.lua
@@ -6,6 +6,7 @@ local action_state = require("telescope.actions.state")
 local action_utils = require("telescope.actions.utils")
 local entry_display = require("telescope.pickers.entry_display")
 local previewers = require("telescope.previewers")
+local sorters = require "telescope.sorters"
 
 local M = {}
 
@@ -90,6 +91,52 @@ function M.show_note_picker(notes, options, cb)
       end,
     })
     :find()
+end
+
+-- Picker definition
+function M.show_note_grep_picker(opts)
+  -- This is how Telescope's `live_grep` does it
+  -- local args = { "rg", "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case" }
+
+  opts = opts or {}
+  -- Prepare the prompt
+  -- TODO: this should be a call to the lua api
+  local args = { "zk", "list", "--format=oneline", "-m" }
+
+  -- Prepare the asynchronous job
+  local live_zk = finders.new_job(
+    function(prompt)
+      -- Empty prompt would give bad results
+      if not prompt or prompt == "" then
+        return nil
+      end
+      return vim.tbl_flatten { args, prompt }
+    end,
+    -- Format entries
+    function(entry)
+      local _, _, title, filename, date_modified = string.find(entry, [[(..-) (%S+) %((.+)%)]])
+      return {
+        value = entry,  -- entire entry is searchable
+        display = title, -- only the title is displayed
+        ordinal = entry, -- TODO: what is this for?
+        -- TODO: remove hardcoded path below
+        path = "~/Areas/notes/" .. filename, -- store the path so we can open the file
+      }
+    end,
+    opts.max_results,
+    opts.cwd)
+
+  pickers.new(opts, {
+    prompt_title = "Live Zk",
+    finder = live_zk,
+    previewer = conf.grep_previewer(opts),
+    sorter = sorters.highlighter_only(opts),
+    attach_mappings = function(_, map)
+      map("i", "<c-space>", actions.to_fuzzy_refine)
+      return true
+    end,
+  })
+  :find()
 end
 
 function M.show_tag_picker(tags, options, cb)
