0cbe7d6a44869a619bd8996f206d177cf7382e35
Pete Kazmier <pete@kazmier.com>
2025-01-11 15:36:26 -0500

    feat: add support for snacks.picker

---
+++ lua/zk/pickers/snacks_picker.lua
@@ -0,0 +1,63 @@
+local M = {}
+local H = {}
+local snacks_picker = require("snacks.picker")
+
+M.note_picker_list_api_selection = { "title", "path", "absPath" }
+
+M.show_note_picker = function(notes, opts, cb)
+  notes = vim.tbl_map(function(note)
+    local title = note.title or note.path
+    return { text = title, file = note.absPath, value = note }
+  end, notes)
+  H.item_picker(notes, opts, cb)
+end
+
+M.show_tag_picker = function(tags, opts, cb)
+  opts.snacks_picker = opts.snacks_picker or {}
+  opts.snacks_picker.preset = "nopreview"
+  local width = H.max_note_count(tags)
+  tags = vim.tbl_map(function(tag)
+    local padded_count = H.pad(tag.note_count, width)
+    return {
+      text = string.format(" %s  %s", padded_count, tag.name),
+      value = tag,
+    }
+  end, tags)
+  H.item_picker(tags, opts, cb)
+end
+
+H.item_picker = function(items, opts, cb)
+  opts = opts or {}
+  local picker_opts = vim.tbl_deep_extend("force", {
+    items = items,
+    format = "text",
+    actions = {
+      confirm = function(picker, item)
+        picker:close()
+        if not opts.multi_select then
+          cb(item.value)
+        else
+          cb(vim.tbl_map(function(i)
+            return i.value
+          end, picker:selected()))
+        end
+      end,
+    },
+  }, opts.snacks_picker or {})
+  snacks_picker.pick(opts.title, picker_opts)
+end
+
+H.max_note_count = function(tags)
+  local max_count = 0
+  for _, t in ipairs(tags) do
+    max_count = math.max(max_count, t.note_count)
+  end
+  return vim.fn.strchars(tostring(max_count))
+end
+
+H.pad = function(text, width)
+  local text_width = vim.fn.strchars(text)
+  return string.rep(" ", width - text_width) .. text
+end
+
+return M
