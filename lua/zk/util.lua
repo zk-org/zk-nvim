c720b1b12c45419806579d9be00b75e66f4aa8a9
Thomas Vandal <thomas.vandal@umontreal.ca>
2025-04-23 09:43:47 -0400

    Fix other instance of `make_given_range_params()`

--- lua/zk/util.lua
+++ lua/zk/util.lua
@@ -59,15 +59,20 @@ local function get_offset_encoding(bufnr)
   return offset_encoding
 end
 
+local function make_range_zk()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local offset_encoding = get_offset_encoding(bufnr)
+  vim.notify(offset_encoding)
+  -- This function has a warning if encoding is not passed
+  return vim.lsp.util.make_given_range_params(nil, nil, bufnr, offset_encoding)
+end
+
 ---Makes an LSP location object from the last selection in the current buffer.
 --
 ---@return table LSP location object
 ---@see https://microsoft.github.io/language-server-protocol/specifications/specification-current/#location
 function M.get_lsp_location_from_selection()
-  local bufnr = vim.api.nvim_get_current_buf()
-  local offset_encoding = get_offset_encoding(bufnr)
-  -- This function has a warning if encoding is not passed
-  local params = vim.lsp.util.make_given_range_params(nil, nil, bufnr, offset_encoding)
+  local params = make_range_zk()
   return {
     uri = params.textDocument.uri,
     range = params.range,
@@ -101,7 +106,7 @@ end
 ---@return table LSP location object
 ---@see https://microsoft.github.io/language-server-protocol/specifications/specification-current/#location
 function M.get_lsp_location_from_caret()
-  local params = vim.lsp.util.make_given_range_params()
+  local params = make_range_zk()
 
   local row, col = unpack(vim.api.nvim_win_get_cursor(0))
   local position = { line = row, character = col }
